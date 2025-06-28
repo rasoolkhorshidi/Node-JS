8aa5b5b9574ac46fab77b9e055a7a67faa0f82f4
rasoolkhorshidi <rasool.khorshidi@gmail.com>
2025-06-28 09:49:58 +0330

    Save Changes

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+const express = require("express");
+const app = express();
 
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", "./pages");
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home Page" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Us" });
+});
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact Us" });
+});
+app.use((req, res) => {
+  res.render("404", { title: "Page Not Found" });
+});
